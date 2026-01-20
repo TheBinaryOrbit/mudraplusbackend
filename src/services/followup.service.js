c631a9306a676e4414badf67d613adf2a8d3ac58
7anish <anissh946@gmail.com>
2026-01-20 17:40:03 +0530

    fix : chage the date fromate

--- src/services/followup.service.js
+++ src/services/followup.service.js
@@ -3,15 +3,16 @@ import Prisma from "../config/prismaClient.js";
 
 export class FollowupService {
     createFollowUp = async ({userId, agentUserId, note, nextFollowUpDate , followUpType , loanId , followUpDate}) => {
+        console.log("Creating follow-up with data:", {userId, agentUserId, note, nextFollowUpDate , followUpType , loanId , followUpDate});
         return await Prisma.followUp.create({
             data: {
                 userId,
                 agentUserId,
                 loanId,
                 note,
-                nextFollowUpDate,
+                nextFollowUpDate: nextFollowUpDate ? new Date(nextFollowUpDate) : null,
                 followUpType,
-                followUpDate,
+                followUpDate: new Date(followUpDate),
             }
         });
     }
